c55ca2da7c770f576c75f3f1bbbee95ba9e470c2
jose0192 <jose.gaspar@correounivalle.edu.co>
2024-04-07 21:23:23 -0500

    Se agregan al archivo el nombre de la materia

--- git.cpp
+++ git.cpp
@@ -2,5 +2,7 @@
 using namespace std;
 int main()
 {
-cout<<"Jose Miguel Gaspar 2372053-3744"<<endl;
+cout<<"Jose Miguel Gaspar"<<endl;
+cout<<"2372053-3744"<<endl;
+cout<<"Metodologias de desarrollo de software"<<endl;
 }
